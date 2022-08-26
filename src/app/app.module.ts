38a863d48c9c81145c9bba9af3e63dee81bf61f3
Yash <yashbhand9@gmail.com>
2022-08-26 14:47:27 +0530

    added student_register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StdRegComponent } from './std-reg/std-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StdRegComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/std-reg/std-reg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-std-reg',
+  templateUrl: './std-reg.component.html',
+  styleUrls: ['./std-reg.component.css']
+})
+export class StdRegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
